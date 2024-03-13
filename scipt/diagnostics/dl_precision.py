9e3a151172d215ac9466c230e927df6ed6c87812
KwintenMissiaen <kwinten.missiaen@kuleuven.be>
2024-03-13 17:59:52 +0100

    add new tests

--- scipt/diagnostics/dl_precision.py
+++ scipt/diagnostics/dl_precision.py
@@ -14,6 +14,7 @@ from opcua import OPCUAConnection
 from configparser import ConfigParser
 import os
 import pickle
+from scipy.signal import hilbert
 
 epoch = datetime.utcfromtimestamp(0)
 
@@ -48,7 +49,7 @@ def fringes_env(dl_pos, ampl, g_delay):
     # Spectrogon Saphire L band
     #wav = 3.7
     #bw  = 0.6 
-    return ampl*np.sinc(2*(dl_pos-g_delay)*bw/wav**2) # See Lawson 2001, Eq 2.7. Factor 2 because delay line postion is twice the OPD
+    return abs(ampl*np.sinc(2*(dl_pos-g_delay)*bw/wav**2)) # See Lawson 2001, Eq 2.7. Factor 2 because delay line postion is twice the OPD
 
 def enveloppe(dl_pos, flx_coh):
     # Define the bins
@@ -66,7 +67,6 @@ def enveloppe(dl_pos, flx_coh):
         pos_min    = dl_min + i*wav
         lim_min    = np.argmin(np.abs(dl_pos - pos_min))
         lim_max    = np.argmin(np.abs(dl_pos - (pos_min+wav)))      
-        #print(lim_min, lim_max)  
         flx_env[i] = np.max(flx_coh[lim_min:lim_max])
         idx_pos    = lim_min + np.argmin(np.abs(flx_coh[lim_min:lim_max] - flx_env[i]))  
         pos_env[i] = dl_pos[idx_pos]
@@ -216,11 +216,26 @@ def read_current_pos(opcua_motor):
     url =  config['DEFAULT']['opcuaaddress']
     opcua_conn = OPCUAConnection(url)
     opcua_conn.connect()
-    target_pos = opcua_conn.read_node('ns=4;s=MAIN.'+opcua_motor+'.ctrl.lrPosition')
+    target_pos = opcua_conn.read_node('ns=4;s=MAIN.'+opcua_motor+'.stat.lrPosActual')
     target_pos = target_pos * 1000
     opcua_conn.disconnect()
 
     return target_pos
+
+def envelop_detector(signal):
+    signal -= signal.mean()
+    analytic_signal = hilbert(signal)
+    flx_env = np.abs(analytic_signal)
+    
+    return flx_env
+
+def save_data(data, path, name):
+    list_saved_files = [elt for elt in os.listdir(save_path) if name in elt]
+    count_file = len(list_saved_files) + 1
+    name_file = name+'_%03d.pkl'%(count_file)
+    dbfile = open(path + name_file, 'wb')
+    pickle.dump(data, dbfile)
+    dbfile.close()
 
 # PLOT of ROI vs time
 # Start animation
@@ -235,8 +250,8 @@ ax_t1.set_ylabel('ROI value')
 # Loop over DL scanning iteration
 dl_id = 1
 speed = 0.05 #mm/s
-wait_time = 0.08 / speed * 2.5 # Time in sec to scan X times the coherent envelope
-grab_range = 0.08 / speed * 5 # Time in sec to scan X times the coherent envelope
+wait_time = 0.08 / speed * 3 # Time in sec to scan X times the coherent envelope
+grab_range = 0.08 / speed * 8 # Time in sec to scan X times the coherent envelope
 
 if dl_id == 2:
     opcua_motor = 'DL_2'
@@ -272,13 +287,15 @@ line_t3, = ax_t1.plot(x, np.sin(x))
 # Loop over DL scans
 margin   = 1
 delay    = rel_pos/speed + margin
-n_pass   = 20 # even number=back and forth
+n_pass   = 2 # even number=back and forth
 null_pos = np.array(range(n_pass), dtype=float)
+# grab_range = delay
 
 null_scans = []
 null_scans_pos = []
 null_scans_best_pos = []
 nb_back_forth = n_pass // 2
+gd_params = []
 
 for it in range(n_pass):
     print('MSG - Pass', it+1, '/', n_pass)
@@ -307,14 +324,15 @@ for it in range(n_pass):
     flx_coh = flux2.copy()
     flx_mean = np.mean(flux2)
     flx_coh = flx_coh - flx_mean
-    print('Scan length:', flx_coh.shape)
 
     # # Save dl_pos and coherent flux
     null_scans.append(flx_coh)
     null_scans_pos.append(dl_pos)
 
     # Find enveloppe
-    pos_env, flx_env = enveloppe(dl_pos, flx_coh)
+    # pos_env, flx_env = enveloppe(dl_pos, flx_coh)
+    flx_env = envelop_detector(flx_coh)
+    pos_env = dl_pos
 
     # Fit group delay to enveloppe
     ampl         = np.abs(np.max(flx_coh)-np.min(flx_coh))/2
@@ -322,12 +340,14 @@ for it in range(n_pass):
     lower_bounds = [0.95*ampl, 1000*dl_start]
     upper_bounds = [1.05*ampl, 1000*dl_end]
     params, params_cov = curve_fit(fringes_env, pos_env, flx_env, p0=init_guess, bounds=(lower_bounds, upper_bounds))
+    print('FIT GD - Minimum value and its position:', flx_coh.min(), dl_pos[np.argmin(flx_coh)])
     print('FIT GD - Fringes amplitude :', params[0])
     print('FIT GD - Group delay [microns]:', params[1])
-    
+    gd_params.append(params)
+   
     # Extract best-fit envelop
     pos_env = dl_pos
-    flx_env = np.abs(fringes_env(pos_env, params[0], params[1]))
+    flx_env = fringes_env(pos_env, params[0], params[1])
 
     # Now fit fringes
     init_guess   = [params[0], params[1], 0.95]
@@ -372,80 +392,81 @@ for it in range(n_pass):
     plt.pause(0.5)
 
 print('MSG - End of pass')
-#######################
-# Set DL to NULL
-#######################
-print('\n*** Set DL to NULL ***')
-current_pos = read_current_pos(opcua_motor)
-print('MSG - Current position:', current_pos)
-print('MSG - Now moving to null position :', null_pos[0])
-cmd_null = (null_pos[0] - current_pos)/1000
-print('Sending command', cmd_null)
-move_rel_dl(cmd_null, speed, opcua_motor)
-#move_abs_dl(null_pos[0]/1000, speed, opcua_motor)
-# Save the last move to check how precise the null is reached
-time.sleep(wait_time)
-to_null_pos, to_null_flx, to_null_flx2 = grab_flux(grab_range, dl_name)
-print('MSG - Reached position', read_current_pos(opcua_motor))
+# #######################
+# # Set DL to NULL
+# #######################
+# print('\n*** Set DL to NULL ***')
+# current_pos = read_current_pos(opcua_motor)
+# print('MSG - Current position:', current_pos)
+# print('MSG - Now moving to null position :', null_pos[0])
+# cmd_null = (null_pos[0] - current_pos)/1000
+# print('Sending command', cmd_null)
+# move_rel_dl(cmd_null, speed, opcua_motor)
+# #move_abs_dl(null_pos[0]/1000, speed, opcua_motor)
+# # Save the last move to check how precise the null is reached
+# time.sleep(wait_time)
+# to_null_pos, to_null_flx, to_null_flx2 = grab_flux(grab_range, dl_name)
+# print('MSG - Reached position', read_current_pos(opcua_motor))
 
-plt.figure()
-plt.plot(to_null_flx2)
-plt.grid()
-plt.xlabel('Time (count)')
-plt.ylabel('Flux (count)')
-plt.title('Reached null position')
+# plt.figure()
+# t_scale = np.linspace(-grab_range, 0., len(to_null_flx2))
+# plt.plot(t_scale, to_null_flx2)
+# plt.grid()
+# plt.xlabel('Time (s)')
+# plt.ylabel('Flux (count)')
+# plt.title('Reached null position')
 
-print('TODO - Close the plot(s) to continue')
-plt.ioff()
-plt.show()
+# print('TODO - Close the plot(s) to continue')
+# plt.ioff()
+# plt.show()
 
-# Go back to starting position when closed
-print('MSG - Moving back to initial position')
-move_abs_dl(dl_start, speed, opcua_motor)
-time.sleep(1.) # the DL overshoot, let it time to reach the targeted position
+# # Go back to starting position when closed
+# print('MSG - Moving back to initial position')
+# move_abs_dl(dl_start, speed, opcua_motor)
+# time.sleep(1.) # the DL overshoot, let it time to reach the targeted position
 
-#################################
-# Set DL to average NULL position
-#################################
-print('\n*** Set DL to average NULL position ***')
-null_scans_best_pos = np.array(null_scans_best_pos)
-null_scans_best_pos = np.reshape(null_scans_best_pos, (2, -1))
+# #################################
+# # Set DL to average NULL position
+# #################################
+# print('\n*** Set DL to average NULL position ***')
+# null_scans_best_pos = np.array(null_scans_best_pos)
+# null_scans_best_pos = np.reshape(null_scans_best_pos, (2, -1))
 
-avg_null_pos = np.mean(null_scans_best_pos[0])
-current_pos = read_current_pos(opcua_motor)
-print('MSG - Current position:', current_pos)
-print('MSG - Now moving to null position :', avg_null_pos)
-cmd_pos = (avg_null_pos - current_pos)/1000
-print('MSG - Sending command', cmd_pos)
-move_rel_dl(cmd_pos, speed, opcua_motor)
-#move_abs_dl(null_pos[0]/1000, speed, opcua_motor)
-# Save the last move to check how precise the null is reached
-time.sleep(wait_time)
-to_null_pos_avg, to_null_flx_avg, to_null_flx_avg2 = grab_flux(grab_range, dl_name)
-print('MSG - Reached position', read_current_pos(opcua_motor))
+# avg_null_pos = np.mean(null_scans_best_pos[0])
+# current_pos = read_current_pos(opcua_motor)
+# print('MSG - Current position:', current_pos)
+# print('MSG - Now moving to null position :', avg_null_pos)
+# cmd_pos = (avg_null_pos - current_pos)/1000
+# print('MSG - Sending command', cmd_pos)
+# move_rel_dl(cmd_pos, speed, opcua_motor)
+# #move_abs_dl(null_pos[0]/1000, speed, opcua_motor)
+# # Save the last move to check how precise the null is reached
+# time.sleep(wait_time)
+# to_null_pos_avg, to_null_flx_avg, to_null_flx_avg2 = grab_flux(grab_range, dl_name)
+# print('MSG - Reached position', read_current_pos(opcua_motor))
 
-plt.figure(figsize=(10, 5))
-plt.subplot(121)
-plt.plot(to_null_flx2)
-plt.grid()
-plt.xlabel('Time (count)')
-plt.ylabel('Flux (count)')
-plt.title('Reached null position 1st scan')
-plt.subplot(122)
-plt.plot(to_null_flx_avg2)
-plt.grid()
-plt.xlabel('Time (count)')
-plt.ylabel('Flux (count)')
-plt.title('Reached null position (average strategy)')
-plt.tight_layout()
+# plt.figure(figsize=(10, 5))
+# plt.subplot(121)
+# plt.plot(to_null_flx2)
+# plt.grid()
+# plt.xlabel('Time (count)')
+# plt.ylabel('Flux (count)')
+# plt.title('Reached null position 1st scan')
+# plt.subplot(122)
+# plt.plot(to_null_flx_avg2)
+# plt.grid()
+# plt.xlabel('Time (count)')
+# plt.ylabel('Flux (count)')
+# plt.title('Reached null position (average strategy)')
+# plt.tight_layout()
 
-print('TODO - Close the plot(s) to continue')
-plt.ioff()
-plt.show()
+# print('TODO - Close the plot(s) to continue')
+# plt.ioff()
+# plt.show()
 
-# Go back to starting position when closed
-print('MSG - Moving back to initial position')
-move_abs_dl(dl_start, speed, opcua_motor)
+# # Go back to starting position when closed
+# print('MSG - Moving back to initial position')
+# move_abs_dl(dl_start, speed, opcua_motor)
 
 ##################################################################################
 # Show results of the scans, individual scan can have different numbers of points
@@ -455,31 +476,85 @@ scans_forth_pos = null_scans_pos[::2]
 scans_back = null_scans[1::2]
 scans_back_pos = null_scans_pos[1::2]
 
-# This plot shows how repeatable a scan is
-plt.figure()
-[plt.plot(scans_forth_pos[i], scans_forth[i], label='Forth') for i in range(len(scans_forth))]
-[plt.plot(scans_back_pos[i], scans_back[i], label='Back') for i in range(len(scans_back))]
-plt.grid()
-plt.xlabel('DL pos ()')
-plt.ylabel('Flux (count)')
-plt.legend(loc='best')
-plt.show()
+# # This plot shows how repeatable a scan is
+# plt.figure()
+# [plt.plot(scans_forth_pos[i], scans_forth[i], label='Forth') for i in range(len(scans_forth))]
+# [plt.plot(scans_back_pos[i], scans_back[i], label='Back') for i in range(len(scans_back))]
+# plt.grid()
+# plt.xlabel('DL pos ()')
+# plt.ylabel('Flux (count)')
+# plt.legend(loc='best')
+# plt.show()
 
-###############
-# Save the data
-###############
+# ###############
+# # Save the data
+# ###############
+# save_path = 'C:/Users/fys-lab-ivs/Documents/Git/NottControl/NOTTControl/scipt/diagnostics/'
+# name_file = 'null_scans_'+opcua_motor+'_speed_%s'%(speed)
+# db = {'scans_forth_pos':scans_forth_pos, 'scans_forth':scans_forth, \
+#       'scans_back_pos':scans_back_pos, 'scans_back':scans_back,\
+#         'null_scans_best_pos': null_scans_best_pos,\
+#             'to_null':[to_null_pos, to_null_flx, to_null_flx2],\
+#                  'to_null_avg':[to_null_pos_avg, to_null_flx_avg, to_null_flx_avg2]}
+
+# save_data(db, save_path, name_file)
+
+# =============================================================================
+# Repeat DL location
+# =============================================================================
+print('\n*** Repeatedly Setting DL to NULL ***')
+
+targeted_pos = null_scans_pos[0][np.argmin(null_scans[0])]
+repeat_null_flx = []
+repeat_null_pos = []
+repeat_reached_pos = []
+n_repeat = 20
+
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10,5))
+ax1.grid()
+ax2.grid()
+
+for k in range(n_repeat):
+    print('Reaching null', k+1, '/', n_repeat)
+    current_pos = read_current_pos(opcua_motor)
+    print('MSG - Current position:', current_pos)
+    print('MSG - Now moving to null position :', targeted_pos)
+    cmd_null = (targeted_pos - current_pos)/1000
+    print('Sending command', cmd_null)
+    move_rel_dl(cmd_null, speed, opcua_motor)
+    
+    # Save the last move to check how precise the null is reached
+    time.sleep(wait_time)
+    to_null_pos, to_null_flx, to_null_flx2 = grab_flux(grab_range, dl_name)
+    repeat_null_pos.append(to_null_pos)
+    repeat_null_flx.append(to_null_flx2)
+    reached_pos = read_current_pos(opcua_motor)
+    print('MSG - Reached position', reached_pos)
+    repeat_reached_pos.append(reached_pos)
+
+    t_scale = np.linspace(-grab_range, 0., len(to_null_flx2))
+    ax1.plot(t_scale, to_null_flx2)
+    ax1.set_xlabel('Time (s)')
+    ax1.set_ylabel('Flux (count)')
+    ax2.plot(to_null_pos, to_null_flx2)
+    ax2.set_xlabel('DL pos (um)')
+    ax2.set_ylabel('Flux (count)')
+    fig.suptitle('Reached null position')
+    
+    # Go back to starting position when closed
+    print('MSG - Moving back to initial position')
+    move_abs_dl(dl_start, speed, opcua_motor)
+    time.sleep(1.) # the DL overshoot, let it time to reach the targeted position
+    print(' ')
+
 save_path = 'C:/Users/fys-lab-ivs/Documents/Git/NottControl/NOTTControl/scipt/diagnostics/'
-list_saved_files = [elt for elt in os.listdir(save_path) if 'null_scans_speed_%s'%(speed) in elt]
-count_file = len(list_saved_files) + 1
-name_file = 'null_scans_'+opcua_motor+'_speed_%s_%03d.pkl'%(speed, count_file)
-dbfile = open(save_path + name_file, 'wb')
-db = {'scans_forth_pos':scans_forth_pos, 'scans_forth':scans_forth, \
+name_file = 'null_repeat_'+opcua_motor+'_speed_%s'%(speed)
+db = {'repeat_null_flx':repeat_null_flx, 'repeat_null_pos':repeat_null_pos,\
+      'gd_params':gd_params,\
+        'scans_forth_pos':scans_forth_pos, 'scans_forth':scans_forth, \
       'scans_back_pos':scans_back_pos, 'scans_back':scans_back,\
-        'null_scans_best_pos': null_scans_best_pos,\
-            'to_null':[to_null_pos, to_null_flx, to_null_flx2],\
-                 'to_null_avg':[to_null_pos_avg, to_null_flx_avg, to_null_flx_avg2]}
+        'targeted_pos':targeted_pos, 'repeat_reached_pos':repeat_reached_pos}
+save_data(db, save_path, name_file)
 
-pickle.dump(db, dbfile)
-dbfile.close()
-
-print(len(null_scans), [len(elt) for elt in null_scans])
+plt.ioff()
+plt.show()
