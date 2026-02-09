Fixing error (missing sqrt(N)) in calculating std on mean. Adding calculation of individual SNR frames, by use of sample std

nottcontrol/camera/diagnostics.py
@@ -79,7 +79,7 @@ class Diagnostics(object):
         sci_frames = self.human_interf.science_frame_sequence(dt)
         dark_frames = self.human_interf.dark_frame_sequence(dt)
         self.dark_frames = dark_frames
-        cal_mean,cal_std,_ = sci_frames.calib(dark_frames)
+        cal_mean,cal_std,_,_ = sci_frames.calib(dark_frames)
         cal_snr = np.divide(cal_mean,cal_std)
         # Identifying outputs
         outputs_pos = self.human_interf.identify_outputs(cal_snr,use_geom,snr_thresh)
@@ -125,14 +125,16 @@ class Diagnostics(object):
         # Fetch science frames
         sci_frames = self.human_interf.science_frame_sequence(dt)
         # Calibration
-        cal_mean,cal_std,cal_seq = sci_frames.calib(self.dark_frames)
+        cal_mean,cal_std,cal_seq,cal_seq_std = sci_frames.calib(self.dark_frames)
         cal_mean_snr = np.divide(cal_mean,cal_std)
+        cal_seq_snr = np.divide(np.transpose(cal_seq,axes=[1,0,2,3]),cal_seq_std)
+        cal_seq_snr = np.transpose(cal_seq_snr,axes=[1,0,2,3])
         # Masking outputs
         cal_mean = cal_mean*self.outputs_pos
         cal_mean_snr = cal_mean_snr*self.outputs_pos
         # Time series of broadband flux: sum output pixels' signal in each frame
         fluxes_broad = cal_seq.sum(axis=(2,3))
-        snrs_broad = cal_seq.sum(axis=(2,3)) # SNR for individual frames TBD
+        snrs_broad = cal_seq_snr.sum(axis=(2,3)) 
         # Dispersed flux: sum output pixels' signal row-per-row in master frame
         flux_disp = cal_mean.sum(axis=2)[self.output_top_idx:self.output_top_idx+self.output_height]
         snr_disp = cal_mean_snr.sum(axis=2)[self.output_top_idx:self.output_top_idx+self.output_height]

nottcontrol/camera/frame.py
@@ -134,16 +134,20 @@ class Frame(object):
         # Calculating the mean over all frames = mean counts per DIT (detector integration time)
         sci_mean = self.av_rois()
         dark_mean = dark.av_rois()
-        # Calculating the sample's standard deviation over all frames, dividing by nr. of frames to get the std on the mean.
-        sci_mean_std = self.std_rois() / N_sci
-        dark_mean_std = dark.std_rois() / N_dark
+        # Calculating the sample's standard deviation over all scientific frames
+        sci_sample_std = self.std_rois()
+        # Dividing by nr. of frames to get the std on the mean.
+        sci_mean_std = sci_sample_std / np.sqrt(N_sci)
+        dark_mean_std = dark.std_rois() / np.sqrt(N_dark)
         # Calibrated master frame, corresponding to one DIT (FLAT TBD)
         cal_mean = sci_mean-dark_mean
-        cal_std = np.sqrt(sci_mean_std**2+dark_mean_std**2)
+        cal_mean_std = np.sqrt(sci_mean_std**2+dark_mean_std**2)
         
         # 2) Calibrated sequence of frames
         cal_seq = np.subtract(np.transpose(self.rois_data,axes=[1,0,2,3]),dark_mean)
         cal_seq = np.transpose(cal_seq,axes=[1,0,2,3])
+        cal_seq_std = np.sqrt(sci_sample_std**2+dark_mean_std**2)
+        
         # SNR for individual frames TBD
-        return cal_mean,cal_std,cal_seq
+        return cal_mean,cal_mean_std,cal_seq,cal_seq_std
         