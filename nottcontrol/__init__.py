5138178b618e201030f349ecb069be44942cdf74
Romain Laugier <romain.laugier@kuleuven.be>
2026-02-19 18:10:35 +0100

    introducing SCIFYsim config file

--- nottcontrol/__init__.py
+++ nottcontrol/__init__.py
@@ -1,7 +1,11 @@
 from pathlib import Path
 import os
 from nottcontrol.config import Config
+from platform import system
 
 parent = Path(__file__).parent
 config_path = os.path.join(parent, "config.ini")
 config = Config(config_path)
+if system() == "Linux":
+    sf_path = config["SCIFYSIM"]["config"]
+    sf_config = Config(sf_path, comment_prefixes="#")
--- nottcontrol/config.py
+++ nottcontrol/config.py
@@ -2,12 +2,57 @@ from configparser import ConfigParser
 import logging
 logit = logging.getLogger(__name__)
 import numpy as np
+from platform import system
+
+def getarray(self, section, key, dtype=np.float64):
+    """
+    An extra get method to parse arrays 
+    
+    **Parameters:**
+    
+    * section   : (str) The section to get the data from
+    * key       : (str) The key of the data
+    * dtype     : A data type for the array conversion
+    """
+    logit.info("Pulling an array from config file")
+    thestring = self[section][key]
+    thelist = thestring.split(sep=",")
+    thearray = np.array( thelist, dtype=dtype)
+    return thearray
+
+ConfigParser.getarray = getarray
+
+def getdate(self, section, key, mode=None):
+    """
+    An extra get method to parse dates in the GENIE .prm format
+    
+    **Parameters:**
+    
+    * section   : (str) The section to get the data from
+    * key       : (str) The key of the data
+    * mode      : In case we need other formats
+    """
+    from astropy.time import Time
+    if mode is not None:
+        raise NotImplementedError("No modes implemented yet")
+    else:
+        logit.info("Pulling an array from config file")
+        rawstring = self[section][key]
+        listargs = rawstring.replace(" ", "").split(",")
+        formated = listargs[0]+"-"+listargs[1]+"-"+listargs[2]+"T"\
+                +listargs[3]+":"+listargs[4]+":"+listargs[5]
+        logit.debug(rawstring)
+        logit.debug(formated)
+        thetime = Time(formated)
+    return thetime
+
+ConfigParser.getdate = getdate
 
 class Config:
     
-    def __init__(self, path:str):
+    def __init__(self, path:str, comment_prefixes=None):
         self._path = path
-        self.config_parser = ConfigParser()
+        self.config_parser = ConfigParser(comment_prefixes=comment_prefixes)
         self.config_parser.optionxform = str # Preserve case sensitivity
         self.config_parser.read(path)
 
@@ -20,47 +65,8 @@ class Config:
     def write(self):
         with open(self._path, 'w') as configfile:
             self.config_parser.write(configfile)
+    def getarray(self, *args, **kwargs):
+        return self.config_parser.getarray(*args, **kwargs)
+    def getdate(self, *args, **kwargs):
+        return self.config_parser.getdate(*args, **kwargs)
             
-    def getarray(self, section, key, dtype=np.float64):
-        """
-        An extra get method to parse arrays 
-        
-        **Parameters:**
-        
-        * section   : (str) The section to get the data from
-        * key       : (str) The key of the data
-        * dtype     : A data type for the array conversion
-        """
-        logit.info("Pulling an array from config file")
-        thestring = self[section][key]
-        thelist = thestring.split(sep=",")
-        thearray = np.array( thelist, dtype=dtype)
-        return thearray
-    
-    ConfigParser.getarray = getarray
-
-    def getdate(self, section, key, mode=None):
-        """
-        An extra get method to parse dates in the GENIE .prm format
-        
-        **Parameters:**
-        
-        * section   : (str) The section to get the data from
-        * key       : (str) The key of the data
-        * mode      : In case we need other formats
-        """
-        from astropy.time import Time
-        if mode is not None:
-            raise NotImplementedError("No modes implemented yet")
-        else:
-            logit.info("Pulling an array from config file")
-            rawstring = self[section][key]
-            listargs = rawstring.replace(" ", "").split(",")
-            formated = listargs[0]+"-"+listargs[1]+"-"+listargs[2]+"T"\
-                    +listargs[3]+":"+listargs[4]+":"+listargs[5]
-            logit.debug(rawstring)
-            logit.debug(formated)
-            thetime = Time(formated)
-        return thetime
-    
-    ConfigParser.getdate = getdate
