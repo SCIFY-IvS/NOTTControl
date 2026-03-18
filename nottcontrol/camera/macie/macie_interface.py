1c917cdea5834273487d4b947012f4fd30336fd2
rlaugier <romain.laugier@kuleuven.be>
2026-03-18 16:25:21 +0100

    Continuous acquisition wip

--- nottcontrol/camera/macie/macie_interface.py
+++ nottcontrol/camera/macie/macie_interface.py
@@ -1,5 +1,6 @@
 import os
 import ctypes
+from threading import Thread, Event
 
 class MacieInterface():
     def __init__(self, offline_mode = True):
@@ -17,6 +18,11 @@ class MacieInterface():
         #Load ctypes dll, and call initialize
         #Should the config file be part of the constructor, or hard-coded?
         self._macielib.M_initialize(b"macie_exe/config_files/basic_warm_slow.cfg", offline_mode)
+
+        self.continuous_acquisition_running = False
+        self._acquiring = threading.Event()
+        self._acquiring.clear()
+        self._closing = threading.Event()
     
     def __enter__(self):
         self.init_camera()
@@ -33,6 +39,10 @@ class MacieInterface():
     
     def init_camera(self):
         self._macielib.M_initCamera()
+
+        #Start the thread for continuous acquisition - it won't execute anything until start_continuous_acquisition is called
+        thread = Thread(target = continuous_acquisition)
+        thread.start()
     
     def acquire(self, no_recon = False):
         self._macielib.M_acquire(no_recon)
@@ -41,10 +51,20 @@ class MacieInterface():
         self._macielib.M_getPower()
     
     def close(self):
+        self._closing.set()
         self._macielib.M_close()
     
     def halt_acquisition(self):
         self._macielib.M_halt_acquisition()
-
     
-
+    def start_continuous_acquisition(self):
+        self._acquiring.set()
+    
+    def stop_continuous_acquisition(self):
+        self._acquiring.clear()
+    
+    def continuous_acquisition(self):
+        #Run for as long as the interface is not closed
+        while not self._closing.is_set():
+            self._acquiring.wait()
+            self.acquire()
