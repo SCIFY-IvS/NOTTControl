335ada78d6e5e0ae66064d65e6546f965407cae3
Thomas MAttheussen <thomas.mattheussen@kuleuven.be>
2026-04-09 17:21:24 +0200

    Correcting accidental remove of parent

--- nottcontrol/script/__init__.py
+++ nottcontrol/script/__init__.py
@@ -3,6 +3,8 @@ import os
 from nottcontrol.config import Config
 from nottcontrol.script.datafiles import DataFiles
 
+_parent = Path(__file__).parent
+
 _config_path_scripts = os.path.join(_parent, "config/config.cfg")
 config_scripts = Config(_config_path_scripts)
 
