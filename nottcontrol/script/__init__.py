b41f42b358df180f3b3e3a76225892483f918fd9
Thomas MAttheussen <thomas.mattheussen@kuleuven.be>
2026-04-09 17:05:27 +0200

    Deprecation of alignment-specific configuration file, merging into config.ini. Config.ini to be tidied WIP

--- nottcontrol/script/__init__.py
+++ nottcontrol/script/__init__.py
@@ -3,10 +3,6 @@ import os
 from nottcontrol.config import Config
 from nottcontrol.script.datafiles import DataFiles
 
-_parent = Path(__file__).parent
-_config_path_alignment = os.path.join(_parent, "cfg/config.cfg")
-config_alignment = Config(_config_path_alignment)
-
 _config_path_scripts = os.path.join(_parent, "config/config.cfg")
 config_scripts = Config(_config_path_scripts)
 
