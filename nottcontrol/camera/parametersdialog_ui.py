96cd9e11acf483f4581678b51365b0fdd1957e27
ddefrere <denis.defrere@kuleuven.be>
2026-07-06 15:19:59 +0200

    Staged changes

--- nottcontrol/camera/parametersdialog_ui.py
+++ nottcontrol/camera/parametersdialog_ui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'c:\Users\fys-lab-ivs\Documents\Git\NottControl\NOTTControl\camera\parametersdialog.ui'
+# Form implementation generated from reading ui file 'c:\Users\fys-lab-ivs\Documents\Git\NottControl\NOTTControl\nottcontrol\camera\parametersdialog.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.9
 #
