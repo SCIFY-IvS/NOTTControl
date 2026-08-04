5fcef62af716a763c284710cfce4d999e34243f6
Thomas MAttheussen <thomas.mattheussen@kuleuven.be>
2026-08-04 10:29:04 +0200

    Mistakingly denoting _save_ref as a static method, removed.

--- nottcontrol/components/human_interface.py
+++ nottcontrol/components/human_interface.py
@@ -746,6 +746,18 @@ class HumInt(object):
         self.push_to_shm(name, frame)
         return frame
 
+    def _save_ref(self, name, beam_nr, x, y, r):
+        """
+        Function that stores a given beam centroid position and dimension in the reference state of lucid_utils and writes it to the config file.
+        """
+        beam_name = "beam"+str(beam_nr)
+        section = "ref_im" if name == "im_cam" else "ref_pup"
+        value = str((round(x, 4), round(y, 4), round(r, 4)))
+        ref_state[name][beam_name] = (x,y,r)
+        config.config_parser.set(section, beam_name, value)
+        config.write()
+        print(f"Reference beam state saved for {beam_name} on camera {name}: x={x:.4f}, y={y:.4f}, r={r:.4f}.")
+        
     def fit_VIS_cam(self, name, beam_nr, visual_feedback=False, save_ref=False):
         """
         Function that fits a beam centroid to a frame acquired by camera {name}. Returns position (x,y) and radius r of beam number {beam_nr}.
@@ -813,19 +825,6 @@ class HumInt(object):
         process.join()
         self._stream_process[name] = None
         print(f"Stream stopped for visible camera {name}.")
-
-    @staticmethod
-    def _save_ref(self, name, beam_nr, x, y, r):
-        """
-        Function that stores a given beam centroid position and dimension in the reference state of lucid_utils and writes it to the config file.
-        """
-        beam_name = "beam"+str(beam_nr)
-        section = "ref_im" if name == "im_cam" else "ref_pup"
-        value = str((round(x, 4), round(y, 4), round(r, 4)))
-        ref_state[name][beam_name] = (x,y,r)
-        config.config_parser.set(section, beam_name, value)
-        config.write()
-        print(f"Reference beam state saved for {beam_name} on camera {name}: x={x:.4f}, y={y:.4f}, r={r:.4f}.")
 
     #------------------#
     # Sample functions |
